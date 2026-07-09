7527c98797b582992c84c309efe17794f8ad7129
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2026-07-09 16:46:16 +0300

    Add common component exports to index file

---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,6 @@
+export { default as CartModal } from './CartModal'
+export { default as Footer } from './Footer'
+export { default as HeroSection } from './HeroSection'
+export { default as NavBar } from './NavBar'
+export { default as JobDetailsModal } from './JobDetailsModal'
+export { default as InvoiceModal } from './InvoiceModal'
